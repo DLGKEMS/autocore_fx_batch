633c61f2270bcc068e08639a4d759216d7fa0279
DLGKEMS <114052384+DLGKEMS@users.noreply.github.com>
2026-04-07 01:30:41 +0900

    Delete processing directory

--- processing/fx/FxRateLookup.java
+++
@@ -1,9 +0,0 @@
-package com.hakjin.autocore.processing.fx;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Optional;
-
-public interface FxRateLookup {
-    Optional<BigDecimal> findKrwPerUnit(String currency, LocalDate postingDate);
-}
